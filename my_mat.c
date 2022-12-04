fe20c215b09f25674cdbc85a71f8ec4b555b9329
Elor Israeli <elorisraeli@gmail.com>
2022-12-04 16:37:16 +0200

    Fix

--- my_mat.c
+++ my_mat.c
@@ -4,51 +4,56 @@
 #define TRUE 1
 #define FALSE 0
 
-void getInput(int mat [N][N]){
+void getInput(int mat[N][N])
+{
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            scanf("%d",&mat[i][j]);
+            scanf("%d", &mat[i][j]);
         }
     }
-    
 }
 
-void floyd_warshall_algorithm(int mat [N][N])
+void floyd_warshall_algorithm(int mat[N][N])
 {
-    
+    int sum;
     for (int k = 0; k < N; k++)
     {
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                if(mat[i][j]>mat[i][k]+mat[k][j])
-                {
-                    mat[i][j]=mat[i][k]+mat[k][j];
-                }
-                
+                sum = mat[i][k]+mat[k][j];
+				if (mat[i][k] == 0 || mat[k][j] == 0) {
+					sum = 0;
+				}
+				if (sum == 0 || i == j || i == k || j == k) {
+					continue;
+				}
+				if (mat[i][j] == 0) {
+                        		mat[i][j] = sum;
+                    		}
+				if (mat[i][j] > sum) {
+					mat[i][j] = sum;
+				}
             }
-            
         }
-        
     }
 }
 
-
-int isExist(int i, int j, int mat [N][N])
+int isExist(int i, int j, int mat[N][N])
 {
-    if(mat[i][j]>0)
+    if (mat[i][j] > 0)
     {
         return TRUE;
     }
     return FALSE;
 }
 
-int short_path(int i, int j, int mat [N][N])
+int short_path(int i, int j, int mat[N][N])
 {
-    if(mat[i][j]>0)
+    if (mat[i][j] > 0)
     {
         return mat[i][j];
     }
